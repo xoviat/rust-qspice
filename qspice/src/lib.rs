3a24f63f0029a9e8b52a7bf5a2507288ee932d43
xoviat <xoviat@users.noreply.github.com>
2026-03-28 19:45:17 -0500

    add ffi vars

--- qspice/src/lib.rs
+++ qspice/src/lib.rs
@@ -1,43 +1,73 @@
 pub use qspice_macros::{main, max, trunc};
 
-use core::ffi::{c_char, c_void};
-use core::ptr;
+use std::ffi::CString;
+use std::io::Write;
 
-#[unsafe(no_mangle)]
-pub static mut Display: *mut c_void = ptr::null_mut(); // works like printf()
-#[unsafe(no_mangle)]
-pub static mut EXIT: *mut c_void = ptr::null_mut(); // print message like printf() but exit(0) afterward
-#[unsafe(no_mangle)]
-pub static mut DegreesC: *const f64 = ptr::null(); // pointer to current circuit temperature
-#[unsafe(no_mangle)]
-pub static mut StepNumber: *const i32 = ptr::null(); // pointer to current step number
-#[unsafe(no_mangle)]
-pub static mut NumberSteps: *const i32 = ptr::null(); // pointer to estimated number of steps
-#[unsafe(no_mangle)]
-pub static mut InstanceName: *const *const c_char = ptr::null(); // pointer to address of instance name
-#[unsafe(no_mangle)]
-pub static mut QUX: *const c_char = ptr::null(); // path to QUX.exe
-#[unsafe(no_mangle)]
-pub static mut ForKeeps: *const bool = ptr::null(); // pointer to whether being evaluated non-hypothetically
-#[unsafe(no_mangle)]
-pub static mut HoldICs: *const bool = ptr::null(); // pointer to whether instance initial conditions are being held
-#[unsafe(no_mangle)]
-pub static mut GUI_HWND: *const c_void = ptr::null(); // pointer to Window handle of QUX.exe
-#[unsafe(no_mangle)]
-pub static mut CKTtime: *const f64 = ptr::null();
-#[unsafe(no_mangle)]
-pub static mut CKTdelta: *const f64 = ptr::null();
-#[unsafe(no_mangle)]
-pub static mut IntegrationOrder: *const i32 = ptr::null();
-#[unsafe(no_mangle)]
-pub static mut InstallDirectory: *const c_char = ptr::null();
-#[unsafe(no_mangle)]
-pub static mut EngAtof: *mut c_void = ptr::null_mut();
-#[unsafe(no_mangle)]
-pub static mut BinaryFormat: *mut c_void = ptr::null_mut(); // BinaryFormat(0x1C) returns "0b00011100"
-#[unsafe(no_mangle)]
-pub static mut EngFormat: *mut c_void = ptr::null_mut(); // EngFormat(1e-6, "s", 6) returns "1µs"
-#[unsafe(no_mangle)]
-pub static mut DFFT: *mut c_void = ptr::null_mut(); // Discrete Fast Fourier Transform
-#[unsafe(no_mangle)]
-pub static mut bzero: *mut c_void = ptr::null_mut();
+mod ffi {
+    use core::ffi::{c_char, c_void};
+    use core::ptr;
+
+    #[unsafe(no_mangle)]
+    pub static mut Display: Option<unsafe extern "C" fn(format: *const i8, ...)> = None;
+    #[unsafe(no_mangle)]
+    pub static mut EXIT: Option<unsafe extern "C" fn(format: *const i8, ...)> = None;
+    #[unsafe(no_mangle)]
+    pub static mut DegreesC: *const f64 = ptr::null(); // pointer to current circuit temperature
+    #[unsafe(no_mangle)]
+    pub static mut StepNumber: *const i32 = ptr::null(); // pointer to current step number
+    #[unsafe(no_mangle)]
+    pub static mut NumberSteps: *const i32 = ptr::null(); // pointer to estimated number of steps
+    #[unsafe(no_mangle)]
+    pub static mut InstanceName: *const *const c_char = ptr::null(); // pointer to address of instance name
+    #[unsafe(no_mangle)]
+    pub static mut QUX: *const c_char = ptr::null(); // path to QUX.exe
+    #[unsafe(no_mangle)]
+    pub static mut ForKeeps: *const bool = ptr::null(); // pointer to whether being evaluated non-hypothetically
+    #[unsafe(no_mangle)]
+    pub static mut HoldICs: *const bool = ptr::null(); // pointer to whether instance initial conditions are being held
+    #[unsafe(no_mangle)]
+    pub static mut GUI_HWND: *const c_void = ptr::null(); // pointer to Window handle of QUX.exe
+    #[unsafe(no_mangle)]
+    pub static mut CKTtime: *const f64 = ptr::null();
+    #[unsafe(no_mangle)]
+    pub static mut CKTdelta: *const f64 = ptr::null();
+    #[unsafe(no_mangle)]
+    pub static mut IntegrationOrder: *const i32 = ptr::null();
+    #[unsafe(no_mangle)]
+    pub static mut InstallDirectory: *const c_char = ptr::null();
+}
+
+pub fn temperature() -> f64 {
+    unsafe { *ffi::DegreesC }
+}
+
+pub fn step_number() -> i32 {
+    unsafe { *ffi::StepNumber }
+}
+
+pub struct Console {}
+
+impl Console {
+    pub const fn new() -> Self {
+        Self {}
+    }
+}
+
+impl Write for Console {
+    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+        // This way ensures that format specifiers in the string are ignored
+        let sp = CString::new("%s")?;
+        let st = CString::new(buf)?;
+
+        unsafe {
+            let fun = ffi::Display.unwrap();
+            fun(sp.as_ptr(), st.as_ptr());
+        }
+
+        Ok(st.count_bytes())
+    }
+
+    fn flush(&mut self) -> std::io::Result<()> {
+        Ok(())
+    }
+}
